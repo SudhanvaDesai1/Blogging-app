903933b886260e4b73e31c54335a9b6d81ce7609
SudhanvaDesai1 <sudhanvadesai1121@gmail.com>
2023-09-21 01:43:47 +0530

    This is the initial update for the blog

--- app.py
+++ app.py
@@ -1,153 +1,365 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, flash, url_for
+from flask_login import LoginManager,UserMixin, current_user
 from flask_sqlalchemy import SQLAlchemy
 import json
 from datetime import datetime
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.sql import func
 
-with open('config.json','r') as c:
+
+
+with open("config.json", "r") as c:
     params = json.load(c)["params"]
 
 app = Flask(__name__)
-app.secret_key = params['secret_key']
-app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+app.secret_key = params["secret_key"]
+app.config["SQLALCHEMY_DATABASE_URI"] = params["local_uri"]
 db = SQLAlchemy(app)
 
+#-------------------- LOGIN MANAGER FROM FLASK LOGIN --------------------------
+
+login_manager = LoginManager()
+login_manager.login_view = "userlogin"
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
+
+
+
+#========================== DATABASE MODELS ==================================
+
+#========================= CONTACT DETAILS OF THE CLIENTS =====================
 class Contacts(db.Model):
+    sno = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    phone_num = db.Column(db.String(15), nullable=False)
+    message = db.Column(db.String(50), nullable=False)
 
-    sno = db.Column(db.Integer(), primary_key = True)
-    name = db.Column(db.String(30), nullable = False)
-    email = db.Column(db.String(30), unique = True, nullable = False)
-    phone_num = db.Column(db.String(15), nullable = False)
-    message = db.Column(db.String(50), nullable = False)
-
+#==================== POSTS OF THE USER OR ADMIN ===========================
 class Posts(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    tagline = db.Column(db.String(30), nullable=False)
+    slug = db.Column(db.String(30), unique=True, nullable=False)
+    content = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.String(20), nullable=True)
+    author = db.Column(db.Integer(),db.ForeignKey('users.id',ondelete='CASCADE'), nullable=False)
+    comments = db.relationship('Comments', backref = 'posts', passive_deletes = True)
+    likess = db.relationship('Likes', backref = 'users', passive_deletes = True)
+# =================== COMMENT SECTION ====================================== 
+class Comments(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author_id = db.Column(db.Integer(),db.ForeignKey('users.id',ondelete='CASCADE'), nullable=False)
+    post_id = db.Column(db.Integer(),db.ForeignKey('posts.id',ondelete='CASCADE'), nullable=False)
 
-    sno = db.Column(db.Integer(), primary_key = True)
-    title = db.Column(db.String(50), nullable = False)
-    tagline = db.Column(db.String(30), nullable = False)
-    slug = db.Column(db.String(30), unique = True, nullable = False)
-    content = db.Column(db.String(50), nullable = False)
-    date = db.Column(db.String(20), nullable = True)
-    img_url = db.Column(db.String(20),nullable = True)
+# ======================USER LIKES MODEL ======================================
+class Likes(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author_id = db.Column(db.Integer(),db.ForeignKey('users.id',ondelete='CASCADE'), nullable=False)
+    post_id = db.Column(db.Integer(),db.ForeignKey('posts.id',ondelete='CASCADE'), nullable=False)
+    
+# ==================== USER DETAILS RELATION IN DATABASE ======================
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(30), unique = True, nullable = False)
+    email = db.Column(db.String(30), unique = True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    postss = db.relationship('Posts', backref = 'users', passive_deletes = True)
+    comments = db.relationship('Comments', backref = 'users', passive_deletes = True)
+    likess = db.relationship('Likes', backref = 'posts', passive_deletes = True)
 
+#-------------------------- DATABASE DESIGN ENDS HERE --------------------------
 
-
-
-
+#========================== URL ROUTES =========================================
+#============================= FOR HOME PAGE ===================================
 @app.route("/")
+@login_required
 def home():
+    userss = Users.query.filter_by().all()
+
+    # username = Users.query.filter_by(user.username).first
     posts = Posts.query.filter_by().all()
-    return render_template("index.html", posts = posts, params=params)
+    return render_template("index1.html", posts=posts, params=params, users = userss, user = current_user)
 
+#=========================== FOR SIGNUP PAGE ===================================
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email_id = request.form.get("email-id")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
 
+        email_exists = Users.query.filter_by(email=email_id).first()
+        username_exists = Users.query.filter_by(username = username).first()
 
+        if email_exists:
+            flash("Email already in use.", category="error")
+        elif username_exists:
+            flash("Username already exists.. ", category='error')
+        elif password1 != password2:
+            flash("Passwords doesn't match. try again",category='error')
+        elif len(username) < 4:
+            flash("Username length must be of minimum 6 characters..",category='warning')
+        elif len(password1) < 8:
+            flash("Password length must be of minimum 8 characters..",category='warning')
+        else:
+            entry = Users(
+                username=username,
+                email=email_id,
+                password=generate_password_hash(password1,method='sha256')
+            )
+            db.session.add(entry)
+            db.session.commit()
+            login_user(entry,remember=True)
+            flash("SignedUp Successfully! Please Login",category='success')
 
+    return render_template("signup.html", params=params)
 
-
-
-@app.route("/contact", methods = ['GET','POST'])
+# ================== FOR CONTACT PAGE ===================================
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
-        phone = request.form.get('phone')
-        message = request.form.get('message')
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        message = request.form.get("message")
 
-        entry = Contacts(name=name,email=email,phone_num=phone,message=message)
+        entry = Contacts(name=name, email=email, phone_num=phone, message=message)
         db.session.add(entry)
         db.session.commit()
 
-    return render_template("contact.html")
+    return render_template("contact.html", params=params, user=current_user)
 
-
-
-
-
-
-
-@app.route("/post/<string:post_slug>", methods = ['GET'])
+# =====================MAIN POSTS PAGE ====================================
+@app.route("/post/<string:post_slug>", methods=["GET"])
 def posts(post_slug):
-    post = Posts.query.filter_by(slug = post_slug).first()
+    post = Posts.query.filter_by(slug=post_slug).first()
 
+    return render_template("post.html", post=post, params=params, user= current_user)
+# ========================= COMMENTS =======================================
+@app.route("/create-comment/<post_id>",methods=["POST"])
+@login_required
+def create_comment(post_id):
+    text=request.form.get('text')
 
-    return render_template("post.html", post = post, params=params)
+    if not text:
+        flash("Comment can't be empty." , category="error")
+    else:
+        post = Posts.query.filter_by(id=post_id).first()
+        comments = Comments.query.filter_by().all()
+        if post:
+            comment = Comments(text=text, author_id = current_user.id, post_id = post_id)
+            db.session.add(comment)
+            db.session.commit()
+    return redirect(url_for("home"))
+#==============================LIKING THE POST===========================
 
+@app.route("/like-post/<post_id>",methods=['GET'])
+@login_required
+def like(post_id):
+    post = Posts.query.filter_by(id=post_id)
+    like = Likes.query.filter_by(author_id=current_user.id, post_id=post_id).first()
+    if like:
+        db.session.delete(like)
+        db.session.commit()
+    else:
+        like = Likes(author_id = current_user.id,post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
+    return redirect(url_for('home'))
 
-
-
-
-
+# ====================== ABOUT PAGE =======================================
 @app.route("/about")
 def about():
-    return render_template("about.html",params=params)
+    return render_template("about.html", params=params)
 
+#-------------------------- ADMIN LEVEL IMPLEMENTATION ----------------------------
 
+# ====================== USER LIST FOR ADMIN======================================== 
+@app.route("/userlist")
+def userlist():
+    users = Users.query.all()
+    return render_template("userlist.html", users=users, params=params,user = current_user)
 
-
-
-
-
-@app.route("/dashboard", methods=['GET','POST'])
+# ========================== ADMIN DASHBOARD =================================
+@app.route("/dashboard", methods=["GET", "POST"])
 def admin():
     posts = Posts.query.all()
-    if 'user' in session and session['user'] == params['admin_user']:
-        return render_template("dashboard.html",params=params, posts = posts)
+
+    if "user" in session and session["user"] == params["admin_user"]:
+        return render_template(
+            "dashboard.html", params=params, posts=posts
+        )
 
     if request.method == "POST":
-        username = request.form.get('uname')
-        userpass = request.form.get('upass')
-        if username == params['admin_user'] and userpass == params['admin_password']:
-            session['user'] = username
-            return render_template("dashboard.html", params=params, posts=posts)
-        
-    return render_template("login.html")
+        username = request.form.get("uname")
+        userpass = request.form.get("upass")
+        if username == params["admin_user"] and userpass == params["admin_password"]:
+            session["user"] = username
+            return render_template(
+                "dashboard.html", params=params, posts=posts, user=current_user
+            )
 
+    return render_template("adminlogin.html")
 
-
-
-@app.route("/edit/<string:sno>", methods=["GET","POST"])
+# ======================= ADMIN POST EDIT AND CREATE PANEL ========================
+@app.route("/edit/<string:sno>", methods=["GET", "POST"])
 def edit(sno):
-    if 'user' in session and session['user'] == params['admin_user']:
+    if "user" in session and session["user"] == params["admin_user"]:
         if request.method == "POST":
             box_title = request.form.get("title")
             tagline = request.form.get("tagline")
             slug = request.form.get("slug")
             content = request.form.get("content")
-            img_file = request.form.get("img_file")
             date = datetime.now()
-            if sno == '0':
-                post = Posts(title = box_title, tagline = tagline, slug = slug, content = content,date = date, img_url = img_file)
+            if sno == "0":
+                post = Posts(
+                    title=box_title,
+                    tagline=tagline,
+                    slug=slug,
+                    content=content,
+                    date=date
+                )
                 db.session.add(post)
                 db.session.commit()
-            else:
-                post = Posts.query.filter_by(sno=sno).first()
-                post.title = box_title
-                post.tagline = tagline
-                post.slug = slug
-                post.content = content
-                post.date = date
-                post.img_url = img_file
-                db.session.commit()
-                return redirect("/edit/"+sno)
-        post = Posts.query.filter_by(sno=sno).first()
-        return render_template("edit.html",params=params,post=post,sno=sno)
-
-
-
+        #     else:
+        #         post = Posts.query.filter_by(sno=sno).first()
+        #         post.title = box_title
+        #         post.tagline = tagline
+        #         post.slug = slug
+        #         post.content = content
+        #         post.date = date
+        #         db.session.commit()
+        #         return redirect("/edit/" + sno)
+        # post = Posts.query.filter_by(sno=sno).first()
+        return render_template("edit.html", params=params, post=post, sno=sno, user=current_user)
+    
+# ========================= LOGS OUT THE ADMIN FROM THE SESSION =======================
 @app.route("/logout")
 def logout():
-    session.pop('user')
+    session.pop("user")
     return redirect("/dashboard")
 
-
-
-@app.route("/delete/<string:sno>", methods = ['GET','POST'])
+# ======================= DELETE THE USER POST FROM ADMIN PANEL =======================
+@app.route("/delete/<string:sno>", methods=["GET", "POST"])
 def delete(sno):
-    if 'user' in session and session['user'] == params['admin_user']:
+    if "user" in session and session["user"] == params["admin_user"]:
         post = Posts.query.filter_by(sno=sno).first()
         db.session.delete(post)
+        db.session.commit()
         return redirect("/dashboard")
 
+#===================== REMOVE THE USER FROM THE ADMIN PANEL ===========================
+@app.route("/deleteuser/<string:user_id>", methods=["GET", "POST"])
+def deleteuser(user_id):
+    if "user" in session and session["user"] == params["admin_user"]:
+        user = Users.query.filter_by(id=user_id).first()
+        db.session.delete(user)
+        db.session.commit()
+        return redirect("/userlist")
 
+#------------------------ ADMIN LEVEL ENDS HERE -------------------------------------
+#------------------------------------------------------------------------------------
+#---------------------- USER LEVEL IMPLEMENTAION STARTS HERE ---------------------------
+
+#================================== USER DASHBOARD =====================================
+@app.route("/userdashboard/<string:usr_id>", methods=["GET", "POST"])
+@login_required
+def userdash(usr_id):
+
+    posts = Posts.query.filter_by(author = usr_id).all()
+    fname = Users.query.get(usr_id).username
+    # passw = Users.query.get(usr_id).password
+    
+    return render_template(
+            "userdashboard.html", params=params, posts=posts, username = fname, user = current_user
+        )
+
+    # if request.method == "POST":
+    #     username = request.form.get("uname")
+    #     userpass = request.form.get("upass")
+    #     if username == fname and userpass == passw:
+    #         session["user"] = fname
+    #         return render_template(
+    #             "userdashboard.html", params=params, posts=posts, username = fname
+    #         )
+
+    # return redirect(url_for('userlogin'))
+#---------------------------------------------------------------------------------
+#===============================User post create=============================
+@app.route('/create-post/<string:sno>', methods = ["GET","POST"])
+@login_required
+def create_post(sno):
+    if request.method == "POST":
+        box_title = request.form.get("title")
+        tagline = request.form.get("tagline")
+        slug = request.form.get("slug")
+        content = request.form.get("content")
+        date = datetime.now()
+        if sno == "0":
+            post = Posts (
+                title=box_title,
+                tagline=tagline,
+                slug=slug,
+                content=content,
+                date=date,
+                author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+        else:
+            post = Posts.query.filter_by(sno=sno).first()
+            post.title = box_title
+            post.tagline = tagline
+            post.slug = slug
+            post.content = content
+            post.date = date
+            db.session.commit()
+            return redirect("/create-post/" + sno)
+    post = Posts.query.filter_by(sno=sno).first()
+    return render_template("userpostedit.html", params=params, post=post, sno=sno, user = current_user)
+#============================================================================
+@app.route("/userlogin", methods = ["GET","POST"])
+def userlogin():
+    if request.method == "POST":
+        email = request.form.get("email")
+        userpass = request.form.get("password")
+        user  = Users.query.filter_by(email = email).first()
+        if user:
+            if check_password_hash(user.password,userpass):
+                flash('User logged in! ')
+                login_user(user, remember=True)
+                return redirect('/userdashboard/'+ str(current_user.id))
+            else:
+                flash("wrong Password!..",category='error')
+        else:
+            flash("User doesnot exist!..",category='error')
+
+    return render_template("userlogin.html")
+#--------------------------------------------------------------------------------
+#==============================USER LOGOUT ========================================
+@app.route("/userlogout")
+@login_required
+def userlogout():
+    logout_user()
+    return redirect("/userlogin")
+
+#---------------------- USER LEVEL IMPLEMENTAION ENDS HERE ---------------------------
+
+
+
+
+
+
+
+#------------------------ THE APP RUNNING METHOD --------------------------------------
 
 if __name__ == "__main__":
     app.run(debug=True)
--- vskublog.sql
+++ vskublog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 19, 2023 at 03:54 PM
+-- Generation Time: Sep 20, 2023 at 09:46 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `vskublog`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `author_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `text`, `date_created`, `author_id`, `post_id`) VALUES
+(1, 'hello there', '2023-09-19 18:04:41', 1, 2),
+(11, 'first comment ', '2023-09-20 07:49:04', 1, 2),
+(12, 'hi manoj', '2023-09-21 00:12:03', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -46,32 +69,82 @@ INSERT INTO `contacts` (`sno`, `name`, `email`, `phone_num`, `message`, `date`) 
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `author_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`id`, `date_created`, `author_id`, `post_id`) VALUES
+(13, '2023-09-21 00:50:46', 3, 2),
+(14, '2023-09-21 00:50:47', 3, 3),
+(18, '2023-09-21 01:03:04', 1, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `posts`
 --
 
 CREATE TABLE `posts` (
-  `sno` int(11) NOT NULL,
-  `title` text NOT NULL,
-  `tagline` text NOT NULL,
-  `slug` varchar(30) NOT NULL,
-  `content` text NOT NULL,
-  `img_url` text NOT NULL,
-  `date` datetime NOT NULL DEFAULT current_timestamp()
+  `id` int(11) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `tagline` varchar(150) NOT NULL,
+  `slug` varchar(150) NOT NULL,
+  `content` varchar(300) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `author` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`sno`, `title`, `tagline`, `slug`, `content`, `img_url`, `date`) VALUES
-(1, 'First-Post', 'Hi ', 'first-post', 'Hello guys this is my first post from the database... !!!!', 'about-bg.jpg', '2023-07-19 19:04:49'),
-(2, 'The black Holes', 'The mystery', 'second-post', 'The Black holes are still the most unexplored area in the field of AstroPhysics.', 'img1.jpg', '2023-07-15 22:31:45'),
-(4, 'General Relativity', 'einstein life time achievement', 'ai', 'This thing talks about the space and time curvature', 'ai.png', '2023-07-16 13:20:43'),
-(5, 'New Post ', 'einstein life time achievement', 'third-post-my', 'The content is simple', 'a1.jpg', '2023-07-19 19:05:43');
+INSERT INTO `posts` (`id`, `title`, `tagline`, `slug`, `content`, `date`, `author`) VALUES
+(2, 'New title', 'A journey towards Machine Learning', 'first-post', 'Hello wvwery one this is my first post from the front end user module', '2023-09-01', 1),
+(3, 'title 1', 'about computers', 'fp\r\n', 'this is the first post from the front end....', '2023-09-15', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'sudhanva', 'sudhanvadesai1121@gmail.com', 'sha256$XGB2R6GMf2fMxEhw$43f2c8005084d63afb5415b35f51e5a7944818b4c82b7f6adc0dc28c5cb0541b'),
+(2, 'manju', 'manju@gmail.com', 'sha256$SQh1ZN5ycQD4ChH0$5484c3eb61718ed3fe86fb6bfe9c1cabe492c46aae4a20cc1241e600d3335d6a'),
+(3, 'newuser', 'newuser@gmail.com', 'sha256$yuR6POlqCuFUE3Sx$ca754bcc2325477e1af7b096b127aa1082dc342d34435edb762b6accf1510d09');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `author_id` (`author_id`),
+  ADD KEY `post_id` (`post_id`);
 
 --
 -- Indexes for table `contacts`
@@ -80,14 +153,35 @@ ALTER TABLE `contacts`
   ADD PRIMARY KEY (`sno`);
 
 --
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `author` (`author_id`),
+  ADD KEY `post` (`post_id`);
+
+--
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`sno`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk` (`author`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `contacts`
@@ -96,10 +190,22 @@ ALTER TABLE `contacts`
   MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
